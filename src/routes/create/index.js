9e2549d8027ce3b779d33456122de6596dcdfe4a
David Giraldo <davidgiraldobueno@gmail.com>
2022-01-24 17:37:02 -0500

    create new task

--- src/routes/create/index.js
+++ src/routes/create/index.js
@@ -3,10 +3,29 @@ import style from './style.css';
 
 const Create = () => {
 
+  const onSubmitform = (e) => {
+    e.prevendDefault();
+    console.log('submit');
+  }
+
   return (
     <div class={style.create}>
       <h1>Create</h1>
-      <p>This is the create page</p>
+      <p>Create a new task</p>
+
+      <form class={style.form} onSubmit={onSubmitform}>
+        <div>
+          <label>Title: </label>
+          <input type="text" name="title" />
+        </div>
+
+        <div>
+          <label>Description: </label>
+          <input type="text" name="description" />
+        </div>
+
+        <button class={style.button} type="submit">Create new task</button>
+      </form>
     </div>
   );
 }
